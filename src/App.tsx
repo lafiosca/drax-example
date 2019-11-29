4adf716f1a3dce77b9c9395fe35cb33b84e61749
Joe Lafiosca <joe@lafiosca.com>
2019-11-29 00:31:35 -0500

    improve example/test

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {
 	SafeAreaView,
 	StyleSheet,
 	StatusBar,
-	Button,
+	Text,
 } from 'react-native';
 
 import { DraxProvider, DraxView } from './Drax';
-
-// const items = [
-// 	'blue',
-// 	'green',
-// 	'red',
-// 	'yellow',
-// 	'cyan',
-// ];
 
 interface Cargo {
 	drag?: number;
@@ -22,7 +14,6 @@ interface Cargo {
 }
 
 const App = () => {
-	const [count, setCount] = useState(0);
 	return (
 		<>
 			<StatusBar barStyle="dark-content" />
@@ -30,7 +21,6 @@ const App = () => {
 				<DraxProvider>
 					<DraxView
 						style={styles.blueBox}
-						receptive
 						onDragStart={() => { console.log('start dragging blue'); }}
 						onDrag={() => { console.log('drag blue'); }}
 						onDragEnd={() => { console.log('stop dragging blue'); }}
@@ -38,18 +28,29 @@ const App = () => {
 						onDragOver={(payload: Cargo) => { console.log(`drag blue over: ${JSON.stringify(payload, null, 2)}`); }}
 						onDragExit={(payload: Cargo) => { console.log(`drag blue out of: ${JSON.stringify(payload, null, 2)}`); }}
 						onDragDrop={(payload: Cargo) => { console.log(`drop blue into: ${JSON.stringify(payload, null, 2)}`); }}
-						dragPayload={{ drag: 1 }}
-					/>
+						dragPayload={{ boxName: 'blue' }}
+					>
+						<Text>draggable only</Text>
+					</DraxView>
 					<DraxView
 						style={styles.greenBox}
 						draggable
 						onReceiveDragDrop={(payload: Cargo) => {
 							console.log(`green received drop: ${JSON.stringify(payload, null, 2)}`);
 						}}
-						receiverPayload={{ receiver: 2 }}
-					/>
-					<Button title="+" onPress={() => setCount(count + 1)} />
-					<Button title="-" onPress={() => setCount(count - 1)} />
+						payload={{ boxName: 'green' }}
+					>
+						<Text>draggable and receptive</Text>
+					</DraxView>
+					<DraxView
+						style={styles.yellowBox}
+						onReceiveDragDrop={(payload: Cargo) => {
+							console.log(`yellow received drop: ${JSON.stringify(payload, null, 2)}`);
+						}}
+						payload={{ boxName: 'yellow' }}
+					>
+						<Text>receptive only</Text>
+					</DraxView>
 				</DraxProvider>
 			</SafeAreaView>
 		</>
@@ -63,31 +64,31 @@ const styles = StyleSheet.create({
 	blueBox: {
 		margin: 12,
 		width: 'auto',
-		height: 300,
+		height: 200,
 		backgroundColor: '#aaaaff',
 	},
 	greenBox: {
 		margin: 12,
 		width: 'auto',
-		height: 300,
+		height: 200,
 		backgroundColor: '#aaffaa',
 	},
 	redBox: {
 		margin: 12,
 		width: 'auto',
-		height: 300,
+		height: 200,
 		backgroundColor: '#ffaaaa',
 	},
 	yellowBox: {
 		margin: 12,
 		width: 'auto',
-		height: 300,
+		height: 200,
 		backgroundColor: '#ffffaa',
 	},
 	cyanBox: {
 		margin: 12,
 		width: 'auto',
-		height: 300,
+		height: 200,
 		backgroundColor: '#aaffff',
 	},
 });
